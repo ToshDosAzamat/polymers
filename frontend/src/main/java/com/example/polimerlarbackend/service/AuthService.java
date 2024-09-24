026564511a03a2d96dc85ae22f3ddbde54b6cc1a
Doston <95841170+ToshDosAzamat@users.noreply.github.com>
2024-09-24 16:12:07 +0500

    Delete frontend directory

--- frontend/src/main/java/com/example/polimerlarbackend/service/AuthService.java
+++
@@ -1,8 +0,0 @@
-package com.example.polimerlarbackend.service;
-
-import com.example.polimerlarbackend.dto.AuthRequest;
-import com.example.polimerlarbackend.dto.AuthResponse;
-
-public interface AuthService {
-    AuthResponse signIn(AuthRequest authRequest);
-}
